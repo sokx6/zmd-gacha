b7c701b347ed1ec636fa1125e6e3abf9006df734
locxl <sokx126@gmail.com>
2026-04-11 14:22:40 +0800

    fix(uid): 修改uid位数

--- internal/utils/uid.go
+++ internal/utils/uid.go
@@ -4,6 +4,6 @@ import "math/rand"
 
 // 生成随机9位UID
 func GenerateUID() uint {
-	// 生成一个随机数，范围在100000000到999999999之间
-	return 100000000 + uint(rand.Intn(900000000))
+	// 生成一个随机数，范围在1000000001到9999999999之间
+	return 1000000001 + uint(rand.Intn(8999999998))
 }
