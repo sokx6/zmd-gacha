82202f95b87cd3bc5554e7a88f9532e05318979a
locxl <sokx126@gmail.com>
2026-04-11 15:56:58 +0800

    fix(gacha): 修复了第一次单抽会导致未查询到UserPool而报错的问题

--- internal/game/database/user.go
+++ internal/game/database/user.go
@@ -1,7 +1,10 @@
 package database
 
 import (
+	"errors"
 	"zmd-gacha/internal/models"
+
+	"gorm.io/gorm"
 )
 
 func (db *Database) GetUserCharacters(uid uint) ([]models.UserCharacter, error) {
@@ -23,6 +26,16 @@ func (db *Database) GetCharFirstInfo(uid uint, characterId uint) (*models.UserCh
 func (db *Database) GetUserPool(uid, poolId uint) (models.UserPool, error) {
 	var userPool models.UserPool
 	if err := db.DB.Where("user_id = ? AND pool_id = ?", uid, poolId).First(&userPool).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			userPool = models.UserPool{
+				UserID: uid,
+				PoolID: poolId,
+			}
+			if createErr := db.DB.Create(&userPool).Error; createErr != nil {
+				return models.UserPool{}, createErr
+			}
+			return userPool, nil
+		}
 		return models.UserPool{}, err
 	}
 	return userPool, nil
